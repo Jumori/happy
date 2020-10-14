6abb8f3d81abb4b31c1cc928518eee9df785503d
Jumori <julianamorikoshi@gmail.com>
2020-10-13 23:40:27 -0300

    Backend - orphanages services

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+
+import routes from './routes';
+
+import './database/connection';
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+});
